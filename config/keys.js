ffbd7598e2179a1d480703f06cf154381ad6172b
Edward Kemp <edward.kemp@W30255MD.local>
2017-08-31 12:57:43 +0100

    changed some config files - authentication and production environments

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//Logic for credentials
+if(process.env.NODE_ENV === 'production') {
+  //return production
+  module.exports = require('./prod')
+}else {
+  //return development - dev keys
+  module.exports = require('./dev')
+}
---
+++ config/prod.js
@@ -0,0 +1,8 @@
+//Production Keys
+//Don't commit this file
+module.exports = {
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  gooogleClientSeceret: process.env.GOOGLE_CLIENT_SECERET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
